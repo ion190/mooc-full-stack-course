e3886821740320972878b7ca09a62768a8f0c673
Ion <ion.iamandii@isa.utm.md>
2024-10-22 14:02:11 +0300

    ex1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,79 +1,31 @@
 import { useState } from "react";
 
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, average, all }) => {
-  return (
-    <>
-      <div>statistics</div>
-      <table>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average / all} />
-        <StatisticLine text="positive" value={(good / all) * 100} />
-      </table>
-    </>
-  );
-};
-
-const FeedbackButton = ({ handleBtn, text }) => {
-  return <button onClick={handleBtn}>{text}</button>;
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [all, setAll] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const handleGoodBtn = () => {
-    setGood(good + 1);
-    setAll(all + 1);
-    setAverage(average + 1);
-  };
+  let [selected, setSelected] = useState(0);
 
-  const handleNeutralBtn = () => {
-    setNeutral(neutral + 1);
-    setAll(all + 1);
-  };
+  const arrLength = anecdotes.length;
 
-  const handleBadBtn = () => {
-    setBad(bad + 1);
-    setAll(all + 1);
-    setAverage(average - 1);
+  const handleBtn = () => {
+    let randomNum = Math.ceil(Math.random() * arrLength) - 1;
+    setSelected(randomNum);
   };
 
   return (
-    <>
-      <div>give feedback</div>
-      <div>
-        <FeedbackButton handleBtn={handleGoodBtn} text="good" />
-        <FeedbackButton handleBtn={handleNeutralBtn} text="neutral" />
-        <FeedbackButton handleBtn={handleBadBtn} text="bad" />
-      </div>
-
-      {all > 0 ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          average={average}
-          all={all}
-        />
-      ) : (
-        <p>No feedback given</p>
-      )}
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleBtn}>next anecdote</button>
+    </div>
   );
 };
 
