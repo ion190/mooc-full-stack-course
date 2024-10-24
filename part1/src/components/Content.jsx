8c364659effff6ed1a8fdc9ece2b49b4546bac3e
Ion <ion.iamandii@isa.utm.md>
2024-10-24 16:20:03 +0300

    ex2.2

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,11 +1,17 @@
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+  let totalEx = 0;
+  for (let i = 0; i < parts.length; i++) {
+    totalEx += Number(parts[i].exercises);
+  }
+
   return (
     <>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p>total of {totalEx} exercises</p>
     </>
   );
 };
